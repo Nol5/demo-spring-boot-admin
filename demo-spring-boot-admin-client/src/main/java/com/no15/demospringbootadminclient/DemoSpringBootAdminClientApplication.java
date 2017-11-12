eb2baf92020d4a727082648a41451ccfe2329a78
No.15 <lertrungmongkol@gmail.com>
2017-11-12 23:32:12 +0700

    Initial demo-spring-boot-admin

---
+++ demo-spring-boot-admin-client/src/main/java/com/no15/demospringbootadminclient/DemoSpringBootAdminClientApplication.java
@@ -0,0 +1,12 @@
+package com.no15.demospringbootadminclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringBootAdminClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringBootAdminClientApplication.class, args);
+	}
+}
